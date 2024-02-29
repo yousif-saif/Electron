527c0155a165313be4344e628646ee5f03da8c53
yousif-saif <mrdento82@gmail.com>
2024-02-29 15:40:17 +0300

    changing the nav bar buttons to go to /html/REQUESTED_FILE.html

--- nav_include.js
+++ nav_include.js
@@ -19,7 +19,7 @@ const navBarHTML = `
                 </a>
             </li>
             <li class="nav-item">
-                <a href="contact.html" class="nav-link">
+                <a href="/html/contact.html" class="nav-link">
                     <svg width="800px" height="800px" viewBox="0 0 1024 1024" class="icon" version="1.1"
                         xmlns="http://www.w3.org/2000/svg">
                         <path fill="currentColor" class="fa-primary"
@@ -36,7 +36,7 @@ const navBarHTML = `
                 </a>
             </li>
             <li class="nav-item">
-                <a href="whoarewe.html" class="nav-link">
+                <a href="/html/whoarewe.html" class="nav-link">
                     <svg width="800px" height="800px" viewBox="0 0 1024 1024" class="icon" version="1.1"
                         xmlns="http://www.w3.org/2000/svg">
                         <path fill="currentColor" class="fa-primary"
@@ -54,7 +54,7 @@ const navBarHTML = `
                 </a>
             </li>
             <li class="nav-item">
-                <a href="portfolio.html" class="nav-link">
+                <a href="/html/portfolio.html" class="nav-link">
                     <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill="currentColor" class="fa-secondary" opacity="0.1" d="M14.4 8.8L14.1577 8.66152C13.6418 8.36676 13 8.73922 13 9.33333L13 14.6667C13 15.2608 13.6418 15.6332 14.1577 15.3385L14.4 15.2L18.6 12.8C19.219 12.4463 19.219 11.5537 18.6 11.2L14.4 8.8Z" />
                         <path fill="currentColor" class="fa-tertiary" opacity="0.1" d="M6.6 8L6.41333 7.88333C5.79807 7.4988 5 7.94112 5 8.66667L5 15.3333C5 16.0589 5.79807 16.5012 6.41333 16.1167L6.6 16L11.4 13C12.139 12.5381 12.139 11.4619 11.4 11L6.6 8Z"/>
@@ -65,7 +65,7 @@ const navBarHTML = `
                 </a>
             </li>
             <li class="nav-item">
-                <a href="projects.html" class="nav-link">
+                <a href="/html/projects.html" class="nav-link">
                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                         width="800px" height="800px" viewBox="0 0 32 32" xml:space="preserve">
                         <g class="fa-group">
